edc25c82708665fde3a77559bc223e701794ce42
tlestang <thibault.lestang@ens-lyon.fr>
2016-09-11 19:11:23 +0200

    topo.cpp modified accordingly. Added method temperatureBC to handle the zero-gradient BC a the boundary

--- topo.cpp
+++ topo.cpp
@@ -7,7 +7,7 @@
 
 
 Topography::Topography(const int d[2], int h, int period,
-		       LatticeSite **sites, LatticeSite **_sites)
+		       VelSite **sites, VelSite **_sites)
 {
   nbNodes = d[0];
 
@@ -25,8 +25,8 @@ Topography::Topography(const int d[2], int h, int period,
       
       for(int y=0;y<nodes[cc][1];y++)
 	{
-	  sites[x][y].setType(LatticeSite::Solid);
-	  _sites[x][y].setType(LatticeSite::Solid);
+	  sites[x][y].setType(VelSite::Solid);
+	  _sites[x][y].setType(VelSite::Solid);
 	}
             
       cc += 1;
@@ -34,7 +34,7 @@ Topography::Topography(const int d[2], int h, int period,
 
 }
 
-void Topography::FreeSlipBC(LatticeSite **sites, LatticeSite **_sites)
+void Topography::FreeSlipBC(VelSite **sites, VelSite **_sites)
 {
   int x,y;
   int op[9] = {0, 1, 4, 3, 2, 6, 5, 8, 7};
@@ -44,21 +44,26 @@ void Topography::FreeSlipBC(LatticeSite **sites, LatticeSite **_sites)
       _sites[x][y].f[6] = _sites[x][y].f[7];
       _sites[x][y].f[2] = _sites[x][y].f[4];
       _sites[x][y].f[5] = _sites[x][y].f[8];
-      // for (int k=0;k<9;k++)
-      // 	{
-      // 	  _sites[x][y].f[k] = sites[x][y].f[op[k]];
-      // 	}
-      //  double ux, uy;
-      // if(x==25)
-      // 	{
-      // 	  ux = uy = 0.0;
-      // 	  for(int k=0;k<9;k++)
-      // 	    {
-      // 	      ux += _sites[x][y].f[k]*LatticeSite::e[k][0];
-      // 	      uy += _sites[x][y].f[k]*LatticeSite::e[k][1];
-      // 	    }
-      // 	  std::cout << "ux = " << ux << " | uy = " << uy << std::endl;
-      // 	}
     }
-  
+}
+
+void Topography::TemperatureBC(VelSite **velSites, ThermalSite **thermalSites,
+				   double **T, double ***u)
+{
+  int x,y; double u0[2] = {0.0, 0.0};
+  double Tcold = 0.0;
+  double TT, pp, uu[2];
+  for (int i=0;i<nbNodes;i++)
+    {
+      x = nodes[i][0]; y = nodes[i][1];
+
+      thermalSites[x][y+1].computeRhoAndU(TT);
+      velSites[x][y+1].computeRhoAndU(pp, uu);
+      
+      for (int k=0;k<4;k++)
+	{
+	  thermalSites[x][y].f[k] = TT/4. + thermalSites[x][y+1].f[k]
+	    - thermalSites[x][y+1].fEq(k,TT,uu);
+	}
+    }
 }
