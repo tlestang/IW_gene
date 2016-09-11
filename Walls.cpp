439aaba3fc368efe9b24e094e2992fe9b92a91fb
tlestang <thibault.lestang@ens-lyon.fr>
2016-09-11 18:49:24 +0200

    Walls.cpp modified so that Walls only treats the TOP wall.

--- Walls.cpp
+++ Walls.cpp
@@ -8,34 +8,20 @@ Boundary::Boundary()
 
 TopWall::TopWall(const int d[2])
 {
-  nbNodes = 2 * d[0];//  + 2* d[1];
+  nbNodes = d[0];
   nodes = new int*[nbNodes];
   for (int i=0;i<nbNodes;i++)
     {nodes[i] = new int[2];}
 
   int cc = 0;
-  for (int x=0;x<d[0];x++)
-    {
-      nodes[cc][0] = x; nodes[cc][1] = 0;
-      cc += 1;
-    }
 
   for (int x=0;x<d[0];x++)
     {
       nodes[cc][0] = x; nodes[cc][1] = d[1]-1;
       cc += 1;
     }
-  // for (int y=0;y<d[1];y++)
-  //   {
-  //     nodes[cc][0] = 0; nodes[cc][1] = y;
-  //     cc +=1;
-  //   }
-  //   for (int y=0;y<d[1];y++)
-  //   {
-  //     nodes[cc][0] = d[0]-1; nodes[cc][1] = y;
-  //     cc +=1;
-  //   }
 }
+
 void TopWall::BoundaryCondition()
 {
 }
