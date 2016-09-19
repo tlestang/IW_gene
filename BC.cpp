b2044ffbc1b782a3f03fcbc1160447a94957572b
tlestang <thibault.lestang@ens-lyon.fr>
2016-09-19 15:40:16 +0200

    Implement temperature BC for top wall and topography in BC.cpp in the same loop as velocity BC.

--- BC.cpp
+++ BC.cpp
@@ -11,12 +11,16 @@ using namespace std;
 void LatticeBoltzmann::BoundaryConditions()
 {
   int op[9] = {0, 3, 4, 1, 2, 7, 8, 5, 6};
+  int c[4][2] = {{1,0}, {0,1}, {-1,0}, {0, -1}};
     for(int x=0;x<dims[0];x++)
     {
       for (int y=0;y<dims[1];y++)
 	{
 	  if(velSites[x][y].isFluidSolid())
 	    {
+	      
+	      // -------- Velocity Boundary Conditions -------------
+	      
 	      nlink = velSites[x][y].getNormalLink();
 	      for(int k=0;k<9;k++)
 		{
@@ -55,6 +59,42 @@ void LatticeBoltzmann::BoundaryConditions()
 					break;
 						}
 		    }
+		}
+
+	      // ----- Temperature Boundary Condition
+
+	      int yp = (y + 1 + dims[1])%dims[1];
+	      double Tcold = 0.0; double eu;
+	      double Tinf, Tsup, rho, ub[2], uinf[2], usup[2];
+	      
+	      if(velSites[x][yp].isSolid())
+		{
+		  thermalSites[x][y-1].computeRhoAndU(Tinf);
+		  velSites[x][y-1].computeRhoAndU(rho, uinf);
+		  velSites[x][y].computeRhoAndU(rho, ub);
+		  
+		  for (int k=0;k<4;k++)
+		    {
+		      eu = c[k][0]*ub[0] + c[k][1]*ub[1];
+		      thermalSites[x][y].f[k] = (Tinf/4.)*(1.+2.*eu)
+			+ thermalSites[x][y-1].f[k]
+			- thermalSites[x][y-1].fEq(k,Tinf,uinf);
+		    }
+		}
+	      else //bottom wall (topography)
+		{
+		  thermalSites[x][y+1].computeRhoAndU(Tsup);
+		  velSites[x][y+1].computeRhoAndU(rho, usup);
+		  velSites[x][y].computeRhoAndU(rho, ub);
+      
+		  for (int k=0;k<4;k++)
+		    {
+		      eu = c[k][0]*ub[0] + c[k][1]*ub[1];
+		      thermalSites[x][y].f[k] = (Tsup/4.)*(1.+ 2.*eu)
+			+ thermalSites[x][y+1].f[k]
+			- thermalSites[x][y+1].fEq(k,Tsup,usup);
+		    }
+	
 		}
 	    }
 	}
