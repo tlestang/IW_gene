daa2fc073bb3aa823e117ac2e4d74dca56d4b04a
tlestang <thibault.lestang@ens-lyon.fr>
2016-09-19 22:35:23 +0200

    Routine LatticeBoltzmann::generateBC() was badly modified is last commit. Now accounts for the line of ghost nodes underneath the topography

--- generateBC.cpp
+++ generateBC.cpp
@@ -11,17 +11,17 @@ using namespace std;
 void LatticeBoltzmann::generateTopography(int h)
 {
   double a; int yy;
-  double uu[2] = {0.0, 0.0};
+  double unull[2] = {0.0, 0.0};
   
-    for (int xx=0;xx<dims[0];xx++)
+    for (int xx=1;xx<dims[0]-1;xx++)
     {
-      a = 2.*M_PI / dims[0];
-      yy = floor(h*sin(a*xx)) + h;
+      a = 2.*M_PI / (dims[0]-2);
+      yy = floor(h*sin(a*(xx-1))) + h + 1; // +1 because there are ghost nodes underneath
       
-      for(int y=0;y<yy;y++)
+      for(int y=1;y<yy;y++)
 	{
-	  velSites[xx][y].init(LatticeSite::Solid, 1.0, uu, 0);
-	  velSites_[xx][y].init(LatticeSite::Solid, 1.0, uu, 0);
+	  velSites[xx][y].init(LatticeSite::Solid, 1.0, unull, 0.0);
+	  velSites_[xx][y].init(LatticeSite::Solid, 1.0, unull, 0.0);
 	}
             
     }
